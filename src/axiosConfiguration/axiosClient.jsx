66958896b4f8bd662f362f67d4dd4f0735c5b8f5
Olabanji <emmanuelojo291@gmail.com>
2025-09-17 15:30:49 +0100

    cookie

--- src/axiosConfiguration/axiosClient.jsx
+++ src/axiosConfiguration/axiosClient.jsx
@@ -1,20 +1,12 @@
 import axios from "axios";
-import { baseURL } from '../utils/environments';
+import { baseURL } from "../utils/environments";
 
 const axiosClient = axios.create({
   baseURL,
+  withCredentials: true, // 👈 send cookies with every request
 });
 
-// Add interceptor to inject token into headers
-axiosClient.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
+// You don’t need the localStorage interceptor anymore
+// Cookies will be sent automatically by axios
 
 export default axiosClient;
